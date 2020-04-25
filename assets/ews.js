4239104dd338631420cdc94fd928cd2077bca47a
erphp <61402373+erphp@users.noreply.github.com>
2020-04-25 19:07:48 +0800

    Update ews.js

--- assets/ews.js
+++ assets/ews.js
@@ -1,7 +1,7 @@
 jQuery(function($){
     $(".ews-button").click(function(){
         var that = $(this),
-            code = $("#ews_code").val();
+            code = that.prev().val();
         if(code){
             if(!that.hasClass("disabled")){
                 that.text("验证中...");
@@ -15,12 +15,24 @@ jQuery(function($){
                     }else{
                         that.removeClass("disabled");
                         that.text("验证登录");
-                        alert("登录失败！");
+                        if(typeof(logtips) != "undefined"){
+                            logtips("登录失败！请检查是否验证码已过期～");
+                        }else if(typeof(layer) != "undefined"){
+                            layer.msg("登录失败！请检查是否验证码已过期～");
+                        }else{
+                            alert("登录失败！请检查是否验证码已过期～");
+                        }
                     }
                 });
             }
         }else{
-            alert("请输入验证码");
+            if(typeof(logtips) != "undefined"){
+                logtips("请输入验证码～");
+            }else if(typeof(layer) != "undefined"){
+                layer.msg("请输入验证码～");
+            }else{
+                alert("请输入验证码～");
+            }
         }
         return false;
     });
