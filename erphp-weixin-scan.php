4b39b95c78f5fb0e9b8ad5f751aa542ebea78d80
erphp <61402373+erphp@users.noreply.github.com>
2020-10-09 13:16:12 +0800

    Update erphp-weixin-scan.php

--- erphp-weixin-scan.php
+++ erphp-weixin-scan.php
@@ -5,10 +5,10 @@ Plugin URI: http://www.mobantu.com/8259.html
 Author: 模板兔
 Author URI: http://www.mobantu.com
 Description: WordPress关注公众号登录，支持未认证的订阅号～
-Version: 2.0
+Version: 2.01
 */
 
-define('EWS_VERSION', '2.0');
+define('EWS_VERSION', '2.01');
 define('EWS_URL', plugins_url('', __FILE__));
 define('EWS_PATH', dirname( __FILE__ ));
 define('EWS_ADMIN_URL', admin_url());
